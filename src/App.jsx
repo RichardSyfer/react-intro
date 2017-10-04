71e45f903096a3fb56679651a6d8b7c9a6507b52
RichardSyfer <deadwalker87@gmail.com>
2017-10-04 19:31:48 +0300

    create react-intro

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Spider',
+  lastName: 'Man'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
